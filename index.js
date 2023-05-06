e0a1451971ac99fa408aeca179e40955eeed7d1b
ERROR <zaynkoshyari1234@gmail.com>
2023-05-06 17:06:57 +0530

    second commit

--- index.js
+++ index.js
@@ -266,32 +266,34 @@
 //     .lastElementChild.innerHTML = '<h1>Hello</h1>'
 // });
 
-const myForm = document.querySelector('#my-form')
-const nameInput = document.querySelector('#name')
-const emailInput = document.querySelector('#email')
-const msg = document.querySelector('.msg')
-const userList = document.querySelector('#users')
+// const myForm = document.querySelector('#my-form')
+// const nameInput = document.querySelector('#name')
+// const emailInput = document.querySelector('#email')
+// const msg = document.querySelector('.msg')
+// const userList = document.querySelector('#users')
 
-myForm.addEventListener('submit', onsubmit)
+// myForm.addEventListener('submit', onsubmit)
 
-function onsubmit(e) {
-    e.preventDefault()
+// function onsubmit(e) {
+//     e.preventDefault()
 
-    if(nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error')
-        msg.innerHTML = 'Please fill out all fields'
+//     if(nameInput.value === '' || emailInput.value === '') {
+//         msg.classList.add('error')
+//         msg.innerHTML = 'Please fill out all fields'
 
-        setTimeout(() => msg.remove(), 3000)
-    } else {
-        const li = document.createElement('li')
-        li.appendChild(document.createTextNode(`${nameInput.value}
-         : ${emailInput.value}`))
+//         setTimeout(() => msg.remove(), 3000)
+//     } else {
+//         const li = document.createElement('li')
+//         li.appendChild(document.createTextNode(`${nameInput.value}
+//          : ${emailInput.value}`))
 
-         userList.appendChild(li)
-         // clear fields
+//          userList.appendChild(li)
+//          // clear fields
 
-         nameInput.value = ''
-         emailInput.value = ''
-    }
+//          nameInput.value = ''
+//          emailInput.value = ''
+//     }
 
-}
+// }
+
+
