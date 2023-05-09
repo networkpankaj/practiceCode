121f564f7cdc459e9224335dc81800110b76e106
ERROR <zaynkoshyari1234@gmail.com>
2023-05-09 08:37:25 +0530

    third commit

--- tipCalculatorProject/index.js
+++ tipCalculatorProject/index.js
@@ -1,6 +1,24 @@
-window.addEventListener("DOMContentLoaded", ()=>{
-    document.getElementById("calculate").addEventListener("click", () => {
-        const amount = parseFloat(document.getElementById("amount").value);
+window.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("calculate").addEventListener("click", () => {
+		const amount = parseFloat(document.getElementById("amount").value);
         
-    })
-})
+        if (isNaN(amount)) return;
+
+		const tipEl = document.getElementById("tip");
+		
+        const tip = Number.parseInt(tipEl.options[tipEl.selectedIndex].value).toFixed();
+
+		const people = Number.parseInt(document.getElementById("people").value);
+
+		const tipInDollars = amount * (tip / 100);
+		const subTotal = parseFloat(amount + tipInDollars);
+		const total = subTotal / people;
+
+		document.querySelector(".info__tip").innerHTML = `⭐️Tip: $${parseFloat(
+			tipInDollars
+		).toFixed()}`;
+		// document.querySelector(".info__total").innerHTML = `💰 Total: $${total} ${
+		// 	people > 1 ? "each" : ""}`;
+		document.querySelector(".info").style = "display: block";
+	});
+});
